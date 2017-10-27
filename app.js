2982f877860f39bf5ca072f6abda934fe792a74c
Kiran <akiran547@gmail.com>
2017-10-27 13:28:56 +0530

    initial commit

---
+++ app.js
@@ -0,0 +1,57 @@
+var express = require('express');
+var path = require('path');
+var favicon = require('serve-favicon');
+var logger = require('morgan');
+var cookieParser = require('cookie-parser');
+var bodyParser = require('body-parser');
+
+var index = require('./routes/index');
+var users = require('./routes/users');
+
+var app = express();
+
+// view engine setup
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'jade');
+
+// uncomment after placing your favicon in /public
+//app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
+app.use(logger('dev'));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(cookieParser());
+app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, './node_modules/e1/public')));
+app.use(express.static(path.join(__dirname, './node_modules/e2/public')));
+app.use(express.static(path.join(__dirname, './node_modules/e3/public')));
+
+app.use('/', index);
+app.use('/users', users);
+
+var e1 = require('e1');
+var e2 = require('e2');
+var e3 = require('e3');
+
+e1.init(app);
+e2.init(app);
+e3.init(app);
+
+// catch 404 and forward to error handler
+app.use(function(req, res, next) {
+  var err = new Error('Not Found');
+  err.status = 404;
+  next(err);
+});
+
+// error handler
+app.use(function(err, req, res, next) {
+  // set locals, only providing error in development
+  res.locals.message = err.message;
+  res.locals.error = req.app.get('env') === 'development' ? err : {};
+
+  // render the error page
+  res.status(err.status || 500);
+  res.render('error');
+});
+
+module.exports = app;
